ff9eb32f75c4e4d997980ecf1a5a297fb4e64cac
kalknord <133772407+kalknord@users.noreply.github.com>
2024-03-12 15:46:08 +0100

    Update Robin und Mo

--- Aufgabe_2/Aufgabe2.py
+++ Aufgabe_2/Aufgabe2.py
@@ -29,11 +29,21 @@ if __name__ == "__main__":
     checkAddress('facebook.com')
     checkAddress('chat.openai.com')
     checkAddress('sam.hwr-berlin.de')
-    checkAddress('www.hwr-berlin.de')
+    checkAddress('hwr-berlin.de')
     checkAddress('github.com')
     checkAddress('spiegel.de')
     checkAddress('tagesschau.de')
     checkAddress('bvg.de')
 
-
+    checkAddress('127.0.0.1')
+    checkAddress('142.251.37.14')
+    checkAddress('86.109.250.101')
+    checkAddress('185.60.217.35')
+    checkAddress('104.18.37.228')
+    checkAddress('194.94.23.253')
+    checkAddress('194.94.22.19')
+    checkAddress('140.82.121.3')
+    checkAddress('128.65.210.8')
+    checkAddress('34.110.152.241')
+    checkAddress('128.65.209.23')
   
