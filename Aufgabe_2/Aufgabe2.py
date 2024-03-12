eee3cf95de1966c7521821a15f06c3c5f18e6188
kalknord <133772407+kalknord@users.noreply.github.com>
2024-03-12 15:39:49 +0100

    Update

--- Aufgabe_2/Aufgabe2.py
+++ Aufgabe_2/Aufgabe2.py
@@ -13,31 +13,27 @@ def resolve_ip_to_hostname(ip):
         return socket.gethostbyaddr(ip)[0]
     except socket.error as err:
         return f"Fehler bei der Auflösung der IP-Adresse: {err}"
-
-def ping(address):
-    command = ['ping', '-c', '4', address] if platform.system() != "Windows" else ['ping', '-n', '4', address]
-    try:
-        output = subprocess.check_output(command, universal_newlines=True)
-        print(output)
-    except subprocess.CalledProcessError as e:
-        print(f"Fehler beim Ping: {e}")
-
-def trace_route(address):
-    command = ['traceroute', address] if platform.system() != "Windows" else ['tracert', address]
-    try:
-        output = subprocess.check_output(command, universal_newlines=True)
-        print(output)
-    except subprocess.CalledProcessError as e:
-        print(f"Fehler bei der Routenverfolgung: {e}")
-
-if __name__ == "__main__":
-    address = input("Gebe IP-Adresse oder Domain ein: ")
-
-    if address.replace('.', '').isdigit():  # Prüfung, ob IP
-        print(f"Domain-Name: {resolve_ip_to_hostname(address)}")
-    else:
-        print(f"IP-Adresse: {resolve_hostname_to_ip(address)}")
     
-    ping(address)
-    print("\nRoute:")
-    trace_route(address)
+def checkAddress (address):
+    if address.replace('.', '').isdigit():  # Prüfung, ob IP
+        print(f"IP-Adresse: {address}, Domain-Name: {resolve_ip_to_hostname(address)}")
+    else:
+        print(f"Domain-Name: {address}, IP-Adresse: {resolve_hostname_to_ip(address)}")
+    
+if __name__ == "__main__":
+    
+    checkAddress('localhost')
+    
+    checkAddress('google.com')
+    checkAddress('kicker.de')
+    checkAddress('facebook.com')
+    checkAddress('chat.openai.com')
+    checkAddress('sam.hwr-berlin.de')
+    checkAddress('www.hwr-berlin.de')
+    checkAddress('github.com')
+    checkAddress('spiegel.de')
+    checkAddress('tagesschau.de')
+    checkAddress('bvg.de')
+
+
+  
