d2d2693dffed9f145fa6fa7f16e15171c4abe2f2
kalknord <133772407+kalknord@users.noreply.github.com>
2024-03-13 10:49:46 +0100

    .

--- Aufgabe_3/Client.py
+++ Aufgabe_3/Client.py
@@ -2,21 +2,18 @@ import socket
 import os
 
 # Set the path for the Unix socket
-socket_path = '/tmp/my_socket'
+server_address = 'time.windows.com'
+port=123
 
 # Create the Unix socket client
-client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Connect to the server
-client.connect(socket_path)
-
-# Send a message to the server
-message = 'Hello from the client!'
-client.sendall(message.encode())
+client.connect((server_address, port))
 
 # Receive a response from the server
-response = client.recv(1024)
-print(f'Received response: {response.decode()}')
+data = client.recv(1024)
+print(f'Received response: {data.decode()}')
 
 # Close the connection
 client.close()
