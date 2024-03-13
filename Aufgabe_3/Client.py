90e216060226dc36d05c6ba4eeb53477ace3d708
kalknord <133772407+kalknord@users.noreply.github.com>
2024-03-13 10:57:26 +0100

    time

--- Aufgabe_3/Client.py
+++ Aufgabe_3/Client.py
@@ -1,19 +1,10 @@
 import socket
 import os
+import ntplib
+from time import ctime
 
-# Set the path for the Unix socket
-server_address = 'time.windows.com'
-port=123
+server = 'time.windows.com'
+ntp_client = ntplib.NTPClient()
+response = ntp_client.request(server, version=3)
+print(ctime(response.tx_time))
 
-# Create the Unix socket client
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Connect to the server
-client.connect((server_address, port))
-
-# Receive a response from the server
-data = client.recv(1024)
-print(f'Received response: {data.decode()}')
-
-# Close the connection
-client.close()
