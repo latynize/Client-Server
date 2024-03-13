c50fe3982cd58e48c2f5c42f3fae4ed9a2c5ee36
kalknord <133772407+kalknord@users.noreply.github.com>
2024-03-13 11:51:21 +0100

    afg4

--- Aufgabe_4/ntp_client.py
+++ Aufgabe_4/ntp_client.py
@@ -3,15 +3,16 @@ import os
 import ntplib
 from time import ctime
 
-server = 'time.windows.com'
-ntp_client = ntplib.NTPClient()
-response = ntp_client.request(server, version=3)
-print(ctime(response.tx_time))
+def getNTP(server):
+    ntp_client = ntplib.NTPClient()
+    response = ntp_client.request(server, version=3)
+    print(ctime(response.tx_time))
 
-server = 'time.apple.com'
-ntp_client = ntplib.NTPClient()
-response = ntp_client.request(server, version=3)
-print(ctime(response.tx_time))
+getNTP('time.apple.com')
+getNTP('time.windows.com')
+getNTP('time.nist.gov')
+getNTP('time.google.com')
+getNTP('time.cloudflare.com')
 
 #List of Daytime Servers
 #https://gist.github.com/mutin-sa/eea1c396b1e610a2da1e5550d94b0453
