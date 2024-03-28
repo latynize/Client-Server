e35643b5e7565e6637483c3c85c9560476e101ab
kalknord <133772407+kalknord@users.noreply.github.com>
2024-03-28 11:18:29 +0100

    reload

--- DB_API/FastAPI/main.py
+++ DB_API/FastAPI/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from databases import Database
 
 app = FastAPI()
+database = Database('postgresql://postgres:post@localhost/postgres')
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+@app.get('/api/personal/')
+async def read_items():
+    try:
+        # Execute SQL query (BE VERY CAREFUL WITH RAW QUERIES!)
+        result = await database.fetch_all(query='SET search_path TO cioban;')
+        result = await database.fetch_all(query='SELECT em.employee_id, em.first_name, em.last_name, em.free_fte, em.e_mail, em.phone_number, em.entry_date, el.exp_lvl_description, t.type_name FROM employee em INNER JOIN experience_level el ON em.experience_level_id = el.experience_level_id INNER JOIN type t ON em.type_id = t.type_id;')
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Query execution error: {str(e)}")
+
