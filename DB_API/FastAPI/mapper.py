60504272d765471bc406ac98e24c976dedf245ce
Z4CKHD <133773382+Z4CKHD@users.noreply.github.com>
2024-04-02 16:58:26 +0200

    model_to_list

--- DB_API/FastAPI/mapper.py
+++ DB_API/FastAPI/mapper.py
@@ -20,26 +20,26 @@ class Mapper:
             await session.execute(text("SET search_path TO cioban"))
             yield session
 
-    def model_to_dict(self, model_instance, exclude_columns=None, order_columns=None, additional_fields=None):
+    def model_to_list(self, model_instance, exclude_columns=None, order_columns=None, additional_fields=None):
         exclude_columns = set(exclude_columns or [])
         order_columns = order_columns or []
         additional_fields = additional_fields or {}
 
-        model_dict = {}
+        model_list = {}
         for column_name in order_columns:
             if column_name in model_instance.__class__.__table__.c and column_name not in exclude_columns:
-                model_dict[column_name] = getattr(model_instance, column_name)
+                model_list[column_name] = getattr(model_instance, column_name)
 
         for key, value in additional_fields.items():
             if key in order_columns:
-                model_dict[key] = value
+                model_list[key] = value
 
-        ordered_dict = {}
+        ordered_list = {}
         for column_name in order_columns:
-            if column_name in model_dict:
-                ordered_dict[column_name] = model_dict[column_name]
+            if column_name in model_list:
+                ordered_list[column_name] = model_list[column_name]
 
-        return ordered_dict
+        return ordered_list
 
 
 
