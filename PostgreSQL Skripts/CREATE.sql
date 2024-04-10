e4d136b4970e7ab3fcf40631b1353d03ac3999d4
RockyIronB <rocky.iron@web.de>
2024-04-10 10:47:18 +0200

    Update CREATE.sql

--- PostgreSQL Skripts/CREATE.sql
+++ PostgreSQL Skripts/CREATE.sql
@@ -157,3 +157,16 @@ INSERT INTO education_degree (education_name) VALUES
 ('Informatik B.A.'),
 ('Informatik M.A.'),
 ('IT-Ausbildung');
+
+-- Create the schema
+CREATE SCHEMA login;
+
+-- Set the search path to the new schema
+SET search_path TO login;
+
+CREATE TABLE user_login (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL
+);
+
