e108e0d577f795ef5b61c5597fe633d08c01eff4
kalknord <133772407+kalknord@users.noreply.github.com>
2024-04-10 15:20:45 +0200

    test fail

--- Backend/test.py
+++ Backend/test.py
@@ -1,10 +1,14 @@
 import pytest
 from httpx import AsyncClient
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import create_async_engine
 from .main import app
 from ORM.mapper import Mapper
 from helper import Helper as h
 import ORM.tables as t
 import main
+
 
 m = Mapper()
 app = main.app
@@ -29,20 +33,20 @@ class test:
         async with async_session() as session:
             yield session
 
-    @pytest.fixture
-    async def test_app():
-        # Create test tables
-        await test_tables()
+            @pytest.fixture
+            async def test_app():
+                # Create test tables
+                await test_tables()
 
-        # Dependency override
-        app.dependency_overrides[app.get_db_session] = override_get_db()
+                # Dependency override
+                app.dependency_overrides[app.get_db_session] = override_get_db(engine)
 
         async with AsyncClient(app=app, base_url="http://test") as ac:
             yield ac  # use httpx AsyncClient for async requests
 
         # Optionally, drop test tables after tests are done
-async def test_root():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Tomato"}
+    async def test_root():
+        async with AsyncClient(app=app, base_url="http://test") as ac:
+            response = await ac.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Tomato"}
