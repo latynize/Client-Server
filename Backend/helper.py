3bde521efd3e5fd77483a808871584988e75a90f
Z4CKHD <133773382+Z4CKHD@users.noreply.github.com>
2024-04-10 11:02:39 +0200

    Update helper.py

--- Backend/helper.py
+++ Backend/helper.py
@@ -1,13 +1,13 @@
 from fastapi import Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from ORM.mapper import Mapper as mapper
+from ORM.mapper import Mapper as m
 
 
 class Helper:
 
     @staticmethod
-    async def universal_delete(model_instance, db: AsyncSession = Depends(mapper.get_db_session), **conditions) -> bool:
+    async def universal_delete(model_instance, db: AsyncSession = Depends(m.get_db_session), **conditions) -> bool:
 
         query = select(model_instance)
         for attr, value in conditions.items():
@@ -25,7 +25,7 @@ class Helper:
         await db.commit()
         return True
 
-    async def universal_insert(model_instance, data: dict, db: AsyncSession = Depends(mapper.get_db_session)):
+    async def universal_insert(model_instance, data: dict, db: AsyncSession = Depends(m.get_db_session)):
         new_entry = model_instance(**data)
         db.add(new_entry)
         try:
@@ -36,7 +36,7 @@ class Helper:
             await db.rollback()
             raise HTTPException(status_code=400, detail=f"Error creating entry: {e}")
         
-    async def universal_update(entity_class, entity_id: int, update_data: dict, db: AsyncSession = Depends(mapper.get_db_session)):
+    async def universal_update(entity_class, entity_id: int, update_data: dict, db: AsyncSession = Depends(m.get_db_session)):
         try:
             entity = await db.get(entity_class, entity_id)
             if not entity:
