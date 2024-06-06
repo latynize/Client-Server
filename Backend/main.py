879d34ff018fc4ab9811ed67397fbd8f85b1c1cd
Z4CKHD <133773382+Z4CKHD@users.noreply.github.com>
2024-06-06 16:38:18 +0200

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -889,7 +889,7 @@ async def search_team_employee(team_id: int, db: AsyncSession = Depends(m.get_db
 async def assign_employee_to_team(team_data: List[t.ConnectionTeamEmployee],db: AsyncSession = Depends(m.get_db_session)):
     """
     Assigns an employee to a team.
-    :param team_data: List of the team and employee ID.
+    :param team_data: List of the team, employee ID and the FTE amount.
     :param db: The database session.
     :return: Success message if the employee was successfully assigned to the team, error message otherwise.
     """
